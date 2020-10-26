f3051d0b1fbc18d670094158c91ee24e9013e36b
luanzixuan <yimuzx@foxmail.com>
2020-10-26 16:03:01 +0800

    修改打包方法

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-26 11:16:34
- * @LastEditTime: 2020-10-26 11:42:56
+ * @LastEditTime: 2020-10-26 15:19:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \ym-bridge-shandian\src\api\data.js
--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2020-10-26 10:52:00
- * @LastEditTime: 2020-10-26 11:05:53
+ * @LastEditTime: 2020-10-26 15:52:33
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \ym-bridge-shandian\src\api\index.js
  */
-import apiReuest from './api-request.js';
-apiReuest.install = function(Vue) {
-    Vue.component(apiReuest.name, apiReuest);
-};
-export default apiReuest;
+import apiRequest from './api-request.js';
+// apiRequest.install = function(Vue) {
+//     Vue.component(apiRequest.name, apiRequest);
+// };
+// console.log(apiRequest)
+// export default apiRequest;
+console.log(apiRequest)
+module.exports = apiRequest
